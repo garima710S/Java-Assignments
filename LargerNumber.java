60a3bb21503e58af780927eca34ac5af40bf2b29
garima710S <gs5685364@gmail.com>
2026-07-11 11:33:53 +0530

    Add LargerNumber class to find maximum of two numbers

---
+++ LargerNumber.java
@@ -0,0 +1,9 @@
+public class LargerNumber {
+    public static void main(String[] args) {
+        int a = 20, b = 35;
+
+        int max = (a > b) ? a : b;
+
+        System.out.println("Larger Number = " + max);
+    }
+}
