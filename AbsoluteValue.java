a674a7a417b22ede78e4f4ba70f6e7d9225f57cd
garima710S <gs5685364@gmail.com>
2026-07-11 11:34:37 +0530

    Add AbsoluteValue class to compute absolute value

---
+++ AbsoluteValue.java
@@ -0,0 +1,9 @@
+public class AbsoluteValue {
+    public static void main(String[] args) {
+        int n = -25;
+
+        int abs = (n < 0) ? -n : n;
+
+        System.out.println("Absolute Value = " + abs);
+    }
+}
